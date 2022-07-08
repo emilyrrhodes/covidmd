1e8bafe2b18a3da678310cc2aab77d2663b2f09c
Emily Rhodes <emily.r.rhodes@colorado.edu>
2022-07-08 11:30:38 -0600

    RMSD files

---
+++ RMSD/rmsd_script.sh
@@ -0,0 +1,20 @@
+#Go into each folder
+
+	#Make index file
+	
+	#Add the right ending to the index file depending on the type of RBD simulated
+
+	#Make the trajectories whole
+
+	#Make the trajectories not jump
+
+	#Calculat the rmsd (Mutation, Loop and Whole) 
+
+	#Rename the files with the name of the mutation
+
+	#Move the .xvg files to the same folder
+
+#End loop
+
+
+
